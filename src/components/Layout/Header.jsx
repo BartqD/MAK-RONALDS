a7c1ac62f51919a0eb5515d83230d5b300aedc61
unknown <bdrozdek@gmail.com>
2023-10-24 10:42:11 +0200

    started "MAK RONALDS" project

---
+++ src/components/Layout/Header.jsx
@@ -0,0 +1,22 @@
+import React from 'react'
+import { Fragment } from 'react'
+import classes from './Header.module.css'
+import logoImage from '../../assets/logo.png'
+
+const Header = () => {
+	return (
+		<Fragment>
+			<header id={classes['main-header']}>
+				<div id={classes['title']}>
+					<img src={logoImage} alt='Flatted hamburger'/>
+					<h1>MAK RONALDS</h1>
+				</div>
+				<nav>
+					<button>Cart (0)</button>
+				</nav>
+			</header>
+		</Fragment>
+	)
+}
+
+export default Header
